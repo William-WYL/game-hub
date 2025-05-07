6665785ab8eacd4a208469a6fa2a5493ba9241ae
Wei(William) Wang <william.wang.uncharted@gmail.com>
2025-05-06 21:56:03 -0500

    Implement dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Grid, GridItem, Show } from '@chakra-ui/react';
 import NavBar from "./components/NavBar";
 
 function App() {
+
   return (
     <Grid templateAreas={{
       base: `"nav" "main"`,

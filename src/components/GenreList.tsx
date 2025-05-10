a1e825f55ab859c4a5edf6170021ebeb03e7d833
Wei(William) Wang <william.wang.uncharted@gmail.com>
2025-05-10 11:44:50 -0500

    Filter games by genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,8 +1,11 @@
-import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
-import useGenres from "../hooks/useGenres";
-import getCroppedImageUrl from "../services/image_url";
+import { Button, HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
+import useGenres, { Genre } from "../hooks/useGenres";
 
-const GenreList = () => {
+interface Props {
+  onSelectGenre: (genre: Genre) => void;
+}
+
+const GenreList = ({ onSelectGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
 
   if (error) return null;
@@ -16,11 +19,17 @@ const GenreList = () => {
             <Image
               boxSize='32px'
               borderRadius={8}
-              src={getCroppedImageUrl(genre.image_background)} />
-            <Text fontSize='lg'>{genre.name}</Text>
+              src={genre.image_background} />
+            <Button
+              onClick={() => onSelectGenre(genre)}
+              fontSize='lg'
+              variant='link'>
+              {genre.name}
+            </Button>
           </HStack>
-        </ListItem>))}
-    </List>
+        </ListItem>))
+      }
+    </List >
   );
 };
 
